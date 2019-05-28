5afde7457681670458ead9c819c49d62a3f15975
Alabandi <mohammad.alabandi@hp.com>
2019-05-28 14:09:42 -0700

    remove commented code and extra bable plugin

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,6 @@ module.exports = {
         loader: 'babel-loader',
         options: {
           presets: ['@babel/preset-env'],
-          plugins: ["@babel/plugin-transform-modules-commonjs"],
           cacheDirectory: true,
         }
       }
